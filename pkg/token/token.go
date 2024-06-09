567ac50a055569b7b0561ae85771dfc529890502
Kory Bailey <bailey.kr@pg.com>
2024-06-08 23:47:58 -0400

    added tokenizer initial logic

---
+++ pkg/token/token.go
@@ -0,0 +1,17 @@
+package token
+
+type Type string
+
+type Token struct {
+    Type    Type
+    Literal string
+}
+
+const (
+    ILLEGAL = "ILLEGAL"
+    EOF     = "EOF"
+    IDENT   = "IDENT"
+    INT     = "INT"
+    ASSIGN  = "="
+    PLUS    = "+"
+)
